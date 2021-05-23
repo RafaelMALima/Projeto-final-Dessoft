bb94da2dba3ca3feade7c5cea8534ec0764bfc51
Christian Delfs <81188253+ChristianDelfs28@users.noreply.github.com>
2021-05-23 13:03:55 -0300

    atualiza_jogo_dessoft_v2
    atualiza o jogo dando animações para andar pra direita e esquerda

--- Jogo_dessoft v_1.py
+++ Jogo_dessoft v_1.py
@@ -3,6 +3,9 @@ from pygame.locals import *
 
 pygame.init()
 
+clock = pygame.time.Clock()
+fps = 60
+
 #resolução do jogo
 largura = 1500
 altura = 1000
@@ -25,11 +28,69 @@ def desenha_grade():
 
 class Jogador ():
     def__init__ (self, x, y):
-        img = pygame.image.load('img/guy1.png')
-        self.image = pygame.tranform.scale(img, (40,80))
+        self.imagens_direita = []
+        self.imagens_esquerda = []
+        self.index = 0
+        self.counter = 0
+        for num in range(1, 5):
+            img_direita = pygame.image.load(f'img/guy{num}.png')
+            img_direita = pygame.tranform.scale(img_direita, (40,80))
+            img_direita = pygame.transform.flip(img_direita, True, False)
+            self.imagens_direita.append(img_direita)
+            self.imagens_esquerda.append(img_esquerda)
+        self.image = self.imagens_direita[self.index]
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+        self.vel_y = 0
+        self.pular = False
+        self.direcao = 0
+        
+def atualiza(self):
+    dx = 0
+    dy = 0
+    cooldown_andar = 5
+    # teclas pressionadas
+    key = pygame.key.get_pressed()
+    if key[pygame.K_SAPCE] and self.pular == False:
+        self.vel_y = -15
+        self.pular = True
+    if key[pygame.K_SPACE] == False:
+        self.pular = False
+    if key[pygame.K_ESQUERDA]:
+        dx -= 5
+        self.counter += 1
+        self.direcao = -1
+    if key[pygame.K_DIREITA]:
+        dx += 5
+        self.counter += 1
+        self.direcao = 1
+    if key[pygame.K_ESQUERDA] == False and key[pygame.K_DIREITA] == False:
+        self.counter = 0
+        self.index = 0
+        if self.direcao == 1:
+            self.image = self.imagens_direita[self.index] 
+        if self.direcao == -1:
+            self.image = self.imagens_esquerda[self.index] 
+        
+    # controla animação
+    if self.counter > cooldown_andar:
+        self.counter = 0        self.index += 1
+        if self.index >= len(self.imagens_direita):
+            self.index = 0
+        if self.direcao == 1:
+            self.image = self.imagens_direita[self.index] 
+        if self.direcao == -1:
+            self.image = self.imagens_esquerda[self.index] 
+    # adiciona gravidade
+    self.vel_y += 1
+    if self.vel_y > 10:
+        self.vel_y = 10
+    dy += self.vel_y
+        
+    
+    
+             
         
 #carrega um dado mapa, e coloca-o na tela. Para adicionar novas coisas ao mapa, basta carregar a imagem, copiar e colar o cófigo da linha 36 à 42, trocando o if por elif, trocar o nome da imagem transformada e adicionar um número correspondente. 
 class Mundo():
@@ -82,7 +143,9 @@ jogador= Jogador(100,screen_height - 130)
 
 jogo = True
 
+
 while jogo == True:
+    clock.tick(fps)
     tela.blit(fundo,(0,0))
     desenha_grade()
     for event in pygame.event.get():
