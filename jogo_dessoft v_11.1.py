d535e1d06d10670979e29571dc8c440e44445077
RafaelMALima <81188798+RafaelMALima@users.noreply.github.com>
2021-06-05 13:44:02 -0300

    Update jogo_dessoft v_11.1.py

--- jogo_dessoft v_11.1.py
+++ jogo_dessoft v_11.1.py
@@ -39,7 +39,7 @@ grupo_espinho = pygame.sprite.Group()
 grupo_inimigo = pygame.sprite.Group()
 grupo_saida = pygame.sprite.Group() 
 
-fundo = pygame.image.load("Assets/Assets/castle.jpg")
+fundo = pygame.image.load("Assets/Fundos/castle.jpg")
 fundo = pygame.transform.scale(fundo,(largura,altura))
 imagem_restart = pygame.image.load("Assets/morte.png")
 imagem_start = pygame.image.load('Assets/comece.png')
